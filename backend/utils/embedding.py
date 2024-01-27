3d32f300510f2a59e788377760ffa1ca60f21e64
Nicholas Le <nicholasl.le224@gmail.com>
2024-01-26 19:02:46 -0800

    Switch Embedding models

--- backend/utils/embedding.py
+++ backend/utils/embedding.py
@@ -4,9 +4,8 @@ from torch.cuda import is_available
 
 load_dotenv()
 
-# model = SentenceTransformer("BAAI/bge-large-en-v1.5", device='cuda')
 device = 'cuda' if is_available() else 'cpu'
-model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2", device=device)
+model = SentenceTransformer("sentence-transformers/all-distilroberta-v1", device=device)
 
 def generate_desc_embedding(description: str):
     return model.encode(description)
