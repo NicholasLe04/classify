2f8d309f6a55ce973b87d1195538d34e3b86d09e
Nicholas Le <103169500+NicholasLe04@users.noreply.github.com>
2024-01-26 18:02:33 -0800

    Create Frontend (#2)
    * Create Frontend
    
    * Update manifest.json

---
+++ frontend/src/components/CourseSearch.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import "./CourseSearch.css";
+
+function CourseSearch() {
+
+  const [recommendations, setRecommendations] = useState([]);
+
+  function getRecommendations(prompt) {
+    console.log(prompt)
+    fetch(
+      "http://localhost/api/v1/get-courses", 
+      {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "user_query": prompt,
+          "limit": 5
+        })
+      }
+    )
+    .then(response => response.json())
+    .then(content => setRecommendations(content['courses']))
+  }
+
+  function handleSearch(prompt) {
+    if (prompt === "") {
+      setRecommendations([])
+      return
+    }
+    getRecommendations(prompt);
+  }
+
+  return (
+    <div className='course-search'>
+      <input 
+        id='course-search-bar'
+        placeholder='I love to...' 
+        type='text'
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSearch(e.target.value);
+          }
+        }}
+      />
+      <ul>
+        {
+          recommendations.map((course) => {
+            return (
+              <li>
+                <p className='course-code'>{course.course_code}</p>
+                <p>{course.course_description}</p>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default CourseSearch
